530f90a23498e4470169650951484c28ba6eadb6
Hậu Trần <hautrantrung.02@gmail.com>
2025-02-26 00:43:33 +0700

    add library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,63 @@
+import { Search, ShoppingBag } from "lucide-react";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "~/components/ui/carousel";
 import Image from "next/image";
+import { Card } from "~/components/ui/card";
+import "./page.scss";
 
 export default function Home() {
+  const data = {
+    landingImgs: [
+      "/img/409887763_369408025613358_2664442766672702811_n.jpg",
+      "/img/426545516_402190502335110_6131758045816123032_n.jpg",
+      "/img/435516940_436443415576485_7457421840932232507_n.jpg",
+      "/img/426545516_402190502335110_6131758045816123032_n.jpg",
+      "/img/435671033_437384438815716_2304331996105511578_n.jpg",
+      "/img/454847799_508972181656941_3504975550377581801_n.jpg",
+    ],
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+    <div id="homePage" className="home-page h-screen overflow-auto">
+      <header
+        id="homeHeader"
+        className="flex justify-between items-center sticky top-0 h-20 px-16 border-b"
+      >
+        <div className="header-branch">
+          <h4 className="font-bold">Hautrank2</h4>
+        </div>
+        <div className="header-search w-[60%] px-16">
+          <div className="flex items-center space-x-4">
+            <Input />
+            <Search />
+          </div>
+        </div>
+        <div className="header-extra">
+          <Button size={"icon"} variant={"ghost"}>
+            <ShoppingBag />
+          </Button>
+        </div>
+      </header>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start my-8">
+        <section className="section-landing h-[80vh] flex w-full">
+          {data.landingImgs.map((imgUrl, index) => {
+            return (
+              <div key={imgUrl + index} className="h-100 w-100 relative">
+                <Image src={imgUrl} alt={imgUrl} width={400} height={400} />
+              </div>
+            );
+          })}
+        </section>
+        <section className="section-category"></section>
+      </main>
+      <footer className="fixed py-2 inset-x-0 bottom-0 row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        Hautrank2
       </footer>
     </div>
   );
